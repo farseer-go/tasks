4d92501eef5b4e187efc3db5230bb44d907c9a35
steden <1470804@qq.com>
2022-12-31 19:27:23 +0800

    race测试通过

--- test/run_test.go
+++ test/run_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/farseer-go/tasks"
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 	"time"
 )
@@ -15,14 +16,20 @@ func TestRun(t *testing.T) {
 		}, context.Background())
 	})
 
+	lock := &sync.Mutex{}
 	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
 	defer cancel()
 	count := 0
 	tasks.Run("testRun", 10*time.Millisecond, func(context *tasks.TaskContext) {
+		lock.Lock()
+		defer lock.Unlock()
 		count++
 	}, ctx)
 
 	time.Sleep(50 * time.Millisecond)
+	lock.Lock()
+	defer lock.Unlock()
+
 	assert.Equal(t, 4, count)
 }
 
@@ -36,12 +43,19 @@ func TestRunNow(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
 	defer cancel()
 
+	var lock sync.Mutex
 	count := 0
 	tasks.RunNow("testRunNow", 10*time.Millisecond, func(context *tasks.TaskContext) {
+		lock.Lock()
+		defer lock.Unlock()
+
 		count++
 	}, ctx)
 
 	time.Sleep(50 * time.Millisecond)
+
+	lock.Lock()
+	defer lock.Unlock()
 
 	assert.Equal(t, 5, count)
 }
@@ -65,22 +79,36 @@ func TestSetNextDuration(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
 	defer cancel()
 
+	var lock sync.Mutex
 	tasks.Run("testRun", 20*time.Millisecond, func(context *tasks.TaskContext) {
+		lock.Lock()
+		defer lock.Unlock()
+
 		context.SetNextDuration(10 * time.Millisecond)
 		count++
 	}, ctx)
 	time.Sleep(50 * time.Millisecond)
+	lock.Lock()
+	defer lock.Unlock()
+
 	assert.Equal(t, 3, count)
 }
 
 func TestSetNextTime(t *testing.T) {
+	var lock sync.Mutex
 	count := 0
 	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
 	tasks.Run("testRun", 20*time.Millisecond, func(context *tasks.TaskContext) {
+		lock.Lock()
+		defer lock.Unlock()
+
 		context.SetNextTime(time.Now().Add(10 * time.Millisecond))
 		count++
 	}, ctx)
 	time.Sleep(50 * time.Millisecond)
 	cancel()
+	lock.Lock()
+	defer lock.Unlock()
+	
 	assert.Equal(t, 3, count)
 }
