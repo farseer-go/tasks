3f6c90a49e9252e6ecc1418e0cadb2478dd7bd6c
steden <1470804@qq.com>
2022-12-31 18:23:06 +0800

    测试覆盖率为100%

--- test/run_test.go
+++ test/run_test.go
@@ -2,21 +2,85 @@ package test
 
 import (
 	"context"
-	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/tasks"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
 func TestRun(t *testing.T) {
-	tasks.Run("testRun", 1*time.Second, testRunFN, context.Background())
-	time.Sleep(5 * time.Second)
-}
-func TestRunNow(t *testing.T) {
-	tasks.RunNow("testRun", 1*time.Second, testRunFN, context.Background())
-	time.Sleep(5 * time.Second)
+	assert.Panics(t, func() {
+		tasks.Run("testRun", 0, func(context *tasks.TaskContext) {
+
+		}, context.Background())
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+	count := 0
+	tasks.Run("testRun", 10*time.Millisecond, func(context *tasks.TaskContext) {
+		count++
+	}, ctx)
+
+	time.Sleep(50 * time.Millisecond)
+	assert.Equal(t, 4, count)
 }
 
-func testRunFN(context *tasks.TaskContext) {
-	flog.Info("doing....")
+func TestRunNow(t *testing.T) {
+	assert.Panics(t, func() {
+		tasks.RunNow("testRunNow", 0, func(context *tasks.TaskContext) {
+
+		}, context.Background())
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	count := 0
+	tasks.RunNow("testRunNow", 10*time.Millisecond, func(context *tasks.TaskContext) {
+		count++
+	}, ctx)
+
+	time.Sleep(50 * time.Millisecond)
+
+	assert.Equal(t, 5, count)
+}
+
+func TestRunPanic(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	tasks.RunNow("testRun", 500*time.Millisecond, func(context *tasks.TaskContext) {
+		context.SetNextDuration(0)
+	}, ctx)
+	tasks.RunNow("testRun", 500*time.Millisecond, func(context *tasks.TaskContext) {
+		context.SetNextTime(time.Now().Add(-1 * time.Second))
+	}, ctx)
+
+	time.Sleep(50 * time.Millisecond)
+}
+
+func TestSetNextDuration(t *testing.T) {
+	count := 0
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	tasks.Run("testRun", 20*time.Millisecond, func(context *tasks.TaskContext) {
+		context.SetNextDuration(10 * time.Millisecond)
+		count++
+	}, ctx)
+	time.Sleep(50 * time.Millisecond)
+	assert.Equal(t, 3, count)
+}
+
+func TestSetNextTime(t *testing.T) {
+	count := 0
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	tasks.Run("testRun", 20*time.Millisecond, func(context *tasks.TaskContext) {
+		context.SetNextTime(time.Now().Add(10 * time.Millisecond))
+		count++
+	}, ctx)
+	time.Sleep(50 * time.Millisecond)
+	cancel()
+	assert.Equal(t, 3, count)
 }
